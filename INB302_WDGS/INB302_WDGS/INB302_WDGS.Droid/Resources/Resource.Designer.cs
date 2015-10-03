12d6a92eb9eba0ae46620912b2e97cf6bfe7092a
DawnMeh <dawn2687@gmail.com>
2015-10-03 19:59:26 +1000

    Adding images
    Adding the background images and logo to the iOS and Android Resources
    folders

--- INB302_WDGS/INB302_WDGS/INB302_WDGS.Droid/Resources/Resource.Designer.cs
+++ INB302_WDGS/INB302_WDGS/INB302_WDGS.Droid/Resources/Resource.Designer.cs
@@ -45,10 +45,10 @@ namespace INB302_WDGS.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int icon = 2130837504;
+			public const int background = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int image = 2130837505;
+			public const int icon = 2130837505;
 			
 			// aapt resource value: 0x7f020002
 			public const int logo = 2130837506;
