0cfebc98cd904fca1396c315357da6d5126570ac
EclipseSystems <uni_dream@yahoo.com>
2015-10-12 00:16:54 +1000

    Uploading Mitchell's screens
    Screens for Twitter, YouTube and Google Maps interfaces.

---
+++ INB302_WDGS/INB302_WDGS/INB302_WDGS/TwitterScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace INB302_WDGS
+{
+	public class TwitterScreen : ContentPage
+	{
+		public TwitterScreen ()
+		{
+			Content = new StackLayout { 
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
