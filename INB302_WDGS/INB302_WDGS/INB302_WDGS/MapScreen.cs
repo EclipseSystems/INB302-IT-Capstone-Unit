9dcc2e30d4e1465afc72fd880cc15ca222d34664
EclipseSystems <uni_dream@yahoo.com>
2015-10-23 22:51:08 +1000

    MapScreen FULL VERSION + geolocation + pin
    YouTube demonstrator screen pending

--- INB302_WDGS/INB302_WDGS/INB302_WDGS/MapScreen.cs
+++ INB302_WDGS/INB302_WDGS/INB302_WDGS/MapScreen.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
+using Geolocator.Plugin;
 
 namespace INB302_WDGS
 {
@@ -15,92 +18,124 @@ namespace INB302_WDGS
 
 		public MapScreen ()
 		{
-            //map = new Map {
-            //    IsShowingUser = true,
-            //    VerticalOptions = LayoutOptions.FillAndExpand,
-            //    HorizontalOptions = LayoutOptions.FillAndExpand
-            //};
+			StackLayout logoLayout = new StackLayout {
+				BackgroundColor = Color.Black,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				Padding = new Thickness (4, 0, 0, 0)
+			};
 
-            //var position = new Position (-27.4709695, 153.0255137); //Latitude/longitude
-            //var pin = new Pin {
-            //    Type = PinType.Place,
-            //    Position = position
-            //};
-            //map.Pins.Add (pin);
+			Image logo = new Image {
+				Source = "QutLogoWhite.png",
+				HeightRequest = (App.screenHeight / 12) - 4,
+				HorizontalOptions = LayoutOptions.StartAndExpand,
+				VerticalOptions = LayoutOptions.CenterAndExpand
+			};
 
-            //Image backgroundImage = new Image () {
-            //    Source = "background.png",
-            //};
+			Button getlocation = new Button {
+				Text = String.Format ("Touch to get location.")
+			};
 
-            //Image logo = new Image {
-            //    Source = "QutLogo.png",
-            //    HeightRequest = 45,
-            //    HorizontalOptions = LayoutOptions.StartAndExpand,
-            //    VerticalOptions = LayoutOptions.CenterAndExpand
-            //};
+			logoLayout.Children.Add (logo);
 
-            //StackLayout logoLayout = new StackLayout {
-            //    BackgroundColor = Color.Black,
-            //    HorizontalOptions = LayoutOptions.FillAndExpand,
-            //    VerticalOptions = LayoutOptions.FillAndExpand,
-            //    Padding = new Thickness (2, 0, 0, 0)
-            //};
+			Grid pageGrid = new Grid {
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				BackgroundColor = Color.White,
+				Opacity = 0.8,
+				RowSpacing = 2,
+				ColumnSpacing = 2,
+				IsClippedToBounds = true,
+				Padding = new Thickness (.5, 1, .5, 0),
+				RowDefinitions = {
+					new RowDefinition { Height = 0 },
+					new RowDefinition { Height = App.screenHeight / 12 },
+					new RowDefinition { Height = App.screenHeight - (App.screenHeight / 12) },
+					new RowDefinition { Height = 0 }
+				},
+				ColumnDefinitions = {
+					new ColumnDefinition { Width = 0 },
+					new ColumnDefinition { Width = App.screenWidth / 10 },
+					new ColumnDefinition { Width = App.screenWidth / 5 - 32 },
+					new ColumnDefinition { Width = 0 }
+				}
+			};
 
-            //logoLayout.Children.Add (logo);
+			StackLayout mapContent = new StackLayout {
+				Padding = new Thickness (5, 0, 2, 0),
+				BackgroundColor = Color.Black
+			};
 
-            //Grid pageGrid = new Grid {
-            //    VerticalOptions = LayoutOptions.CenterAndExpand,
-            //    HorizontalOptions = LayoutOptions.CenterAndExpand,
-            //    BackgroundColor = Color.White,
-            //    Opacity = 0.8,
-            //    RowSpacing = 2,
-            //    ColumnSpacing = 2,
-            //    IsClippedToBounds = true,
-            //    Padding = new Thickness (.5, 1, .5, 0),
-            //    RowDefinitions = {
-            //        new RowDefinition { Height = 0 }
-            //    },
-            //    ColumnDefinitions = {
-            //        new ColumnDefinition { Width = 0 }
-            //    }
-            //};
+			//Define map content
+			map = new Map {
+				IsShowingUser = true,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
 
-            //Label backLbl = new Label {
-            //    Text = "<",
-            //    BackgroundColor = Color.Black,
-            //    TextColor = Color.White,
-            //    FontSize = 24,
-            //    XAlign = TextAlignment.Center,
-            //    YAlign = TextAlignment.Center
-            //};
+			var pin = new Pin {
+				Type = PinType.Place,
+				Position = new Position (-27.4773531, 153.0289662),
+				Label = "Old Government House",
+				Address = "2 George Street, Brisbane, QLD 4000"
+			};
+			map.Pins.Add (pin);
 
-            //backLbl.GestureRecognizers.Add (new TapGestureRecognizer {
-            //    Command = new Command (() => goBack ())
-            //});
+			var slider = new Slider (1, 18, 1);
+			slider.ValueChanged += (sender, e) => {
+				var zoomLevel = e.NewValue;
+				var latlongdegrees = 360 / (Math.Pow (2, zoomLevel));
+				Debug.WriteLine (zoomLevel + " -> " + latlongdegrees);
+				if (map.VisibleRegion != null)
+					map.MoveToRegion (new MapSpan (map.VisibleRegion.Center, latlongdegrees, latlongdegrees));
+			};
 
-            //pageGrid.Children.Add (backLbl, 1, 1);
-            //pageGrid.Children.Add (logoLayout, 2, 5, 1, 2);
+			getlocation.Clicked += async (sender, e) => {
+				var loc = CrossGeolocator.Current;
 
-            //RelativeLayout content = new RelativeLayout ();
-            //StackLayout innerContent = new StackLayout {
-            //    HorizontalOptions = LayoutOptions.Center,
-            //    VerticalOptions = LayoutOptions.Center
-            //};
+				loc.DesiredAccuracy = 100;
 
-            //innerContent.Children.Add (pageGrid);
+				var pos = await loc.GetPositionAsync (timeoutMilliseconds: 10000);
 
-            //content.Children.Add (backgroundImage,
-            //    Constraint.Constant (0),
-            //    Constraint.Constant (0),
-            //    Constraint.RelativeToParent ((Parent) => {
-            //        return App.screenWidth;
-            //    }),
-            //    Constraint.RelativeToParent ((Parent) => {
-            //        return App.screenHeight;
-            //    }));
+				map.MoveToRegion (MapSpan.FromCenterAndRadius (
+					new Position (pos), Distance.FromKilometers (0.5)));
+			};
 
-            //this.Content = content;
-            //this.Padding = new Thickness (0, Device.OnPlatform (20, 0, 0), 0, 0);
+			mapContent.Children.Add (map);
+			mapContent.Children.Add (pin);
+			mapContent.Children.Add (slider);
+			//End map content
+
+			Label backLbl = new Label {
+				Text = "<",
+				BackgroundColor = Color.Black,
+				TextColor = Color.White,
+				FontSize = 24,
+				XAlign = TextAlignment.Center,
+				YAlign = TextAlignment.Center
+			};
+
+			backLbl.GestureRecognizers.Add (new TapGestureRecognizer {
+				Command = new Command (() => goBack ())
+			});
+
+			pageGrid.Children.Add (backLbl);
+
+			pageGrid.Children.Add (logoLayout, 2, 7);
+
+			pageGrid.Children.Add (mapContent);
+
+			RelativeLayout content = new RelativeLayout ();
+			StackLayout innerContent = new StackLayout {
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center
+			};
+
+			innerContent.Children.Add (pageGrid);
+
+			this.Content = innerContent;
+			this.Padding = new Thickness (0, Device.OnPlatform (20, 0, 0), 0, 0);
+			this.BackgroundImage = "background.png";
 		}
 
 		private void goBack ()
@@ -109,3 +144,5 @@ namespace INB302_WDGS
 		}
 	}
 }
+
+
